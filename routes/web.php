ede501d5e67a4cdcac4e0f50f5a72e93c95bfa8e
HerdiEhza <herdi.ehza@icloud.com>
2023-11-22 00:28:03 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -17,43 +17,44 @@ Route::view('/', 'welcome');
 Route::view('/multi-step', 'multi-step');
 Route::view('/dynamic-form', 'dynamic-form');
 
-Route::get('/quick-count/dashboard', \App\Livewire\Dashboard::class)
-    ->middleware(['auth'])
-    ->name('dashboard.qc');
-Route::get('/quick-count/dashboard/detail/{caleg}', \App\Livewire\Detail::class)
-    ->middleware(['auth'])
-    ->name('dashboard.qc.detail');
-Route::get('/quick-count/dashboard/detail/{caleg}/dd/{detail_2}', \App\Livewire\DetailDua::class)
-    ->middleware(['auth'])
-    ->name('dashboard.qc.detail.2');
-Route::get('/quick-count/dashboard/detail/{caleg}/dd/{detail_2}/ddd/{detail_3}', \App\Livewire\DetailTiga::class)
-    ->middleware(['auth'])
-    ->name('dashboard.qc.detail.3');
-Route::get('/quick-count/dashboard/detail/{caleg}/dd/{detail_2}/ddd/{detail_3}/dddd/{detail_4}', \App\Livewire\DetailEmpat::class)
-    ->middleware(['auth'])
-    ->name('dashboard.qc.detail.4');
-
-Route::get('/quick-count/input-suara', \App\Livewire\QuickCount\InputSuara::class)
-    ->middleware(['auth'])
-    ->name('qc.input-suara');
-
-Route::get('/timses/dashboard', \App\Livewire\Timses\Dashboard::class)
-    ->middleware(['auth'])
-    ->name('timses.dashboard');
-Route::get('/timses/input-timses', \App\Livewire\Timses\InputTimses::class)
-    ->middleware(['auth'])
-    ->name('timses.input-timses');
-Route::get('/timses/input-relawan', \App\Livewire\Timses\InputRelawan::class)
-    ->middleware(['auth'])
-    ->name('timses.input-relawan');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-
-require __DIR__.'/auth.php';
+Route::prefix('admin')->group(function () {
+    Route::get('/quick-count/dashboard', \App\Livewire\Dashboard::class)
+        ->middleware(['auth'])
+        ->name('dashboard.qc');
+    Route::get('/quick-count/dashboard/detail/{caleg}', \App\Livewire\Detail::class)
+        ->middleware(['auth'])
+        ->name('dashboard.qc.detail');
+    Route::get('/quick-count/dashboard/detail/{caleg}/dd/{detail_2}', \App\Livewire\DetailDua::class)
+        ->middleware(['auth'])
+        ->name('dashboard.qc.detail.2');
+    Route::get('/quick-count/dashboard/detail/{caleg}/dd/{detail_2}/ddd/{detail_3}', \App\Livewire\DetailTiga::class)
+        ->middleware(['auth'])
+        ->name('dashboard.qc.detail.3');
+    Route::get('/quick-count/dashboard/detail/{caleg}/dd/{detail_2}/ddd/{detail_3}/dddd/{detail_4}', \App\Livewire\DetailEmpat::class)
+        ->middleware(['auth'])
+        ->name('dashboard.qc.detail.4');
+    
+    Route::get('/quick-count/input-suara', \App\Livewire\QuickCount\InputSuara::class)
+        ->middleware(['auth'])
+        ->name('qc.input-suara');
+    
+    Route::get('/timses/dashboard', \App\Livewire\Timses\Dashboard::class)
+        ->middleware(['auth'])
+        ->name('timses.dashboard');
+    Route::get('/timses/input-timses', \App\Livewire\Timses\InputTimses::class)
+        ->middleware(['auth'])
+        ->name('timses.input-timses');
+    Route::get('/timses/input-relawan', \App\Livewire\Timses\InputRelawan::class)
+        ->middleware(['auth'])
+        ->name('timses.input-relawan');
+    
+    Route::view('dashboard', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
+    
+    Route::view('profile', 'profile')
+        ->middleware(['auth'])
+        ->name('profile');
+    
+    require __DIR__.'/auth.php';
+});
