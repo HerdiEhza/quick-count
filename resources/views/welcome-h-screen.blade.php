44d28e02af382000ffd8a8fe55d3e2e496127bbc
HerdiEhza <herdi.ehza@icloud.com>
2023-11-26 12:58:04 +0700

    change h-screen to h-full

--- resources/views/welcome-h-screen.blade.php
+++ resources/views/welcome-h-screen.blade.php
@@ -16,9 +16,10 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-full">
-    {{-- <section class="flex flex-col justify-between px-4 pt-4 md:flex-row"> --}}
-    <section class="relative grid w-full h-full" style="min-height: -webkit-fill-available;">
-        <div class="z-40 grid h-full grid-cols-1 px-4 pt-4 md:grid-cols-3" style="min-height: -webkit-fill-available;">
+    {{-- <section class="flex flex-col justify-between w-full h-screen px-4 pt-4 md:flex-row"> --}}
+    <section class="relative grid w-full h-full md:h-screen" style="min-height: -webkit-fill-available;">
+        <div class="z-40 grid h-full grid-cols-1 px-4 pt-4 md:grid-cols-3 md:h-screen"
+            style="min-height: -webkit-fill-available;">
             <img src="{{ asset('assets/foto-H.Miftah.webp') }}" alt="" class="self-end hidden w-full h-auto md:block">
             <div class="flex flex-col items-center text-center">
                 <div class="flex items-center space-x-2 text-center">
@@ -39,6 +40,13 @@
                         MENJEMPUT KEMENANGAN
                     </span>
                 </div>
+                {{-- <div class="hidden mt-14 md:block">
+                    <p class="-mb-5 text-2xl text-white">Pilih</p>
+                    <div class="text-white">
+                        <span class="text-5xl">Nomor</span>
+                        <span class="text-7xl">1</span>
+                    </div>
+                </div> --}}
             </div>
             <img src="{{ asset('assets/foto-H.Miftah.webp') }}" alt="" class="block w-auto h-full md:hidden">
             <div class="flex flex-col justify-end h-auto pb-4 text-center md:text-right">
@@ -50,11 +58,11 @@
                     <div class="flex h-full p-4 m-auto text-center bg-white">
                         <h2 class="text-6xl font-extrabold text-gray-700 md:text-8xl">1</h2>
                     </div>
-                    <div class="flex flex-col w-full h-full">
-                        <div class="flex items-center h-full p-2 text-center bg-white">
+                    <div class="w-full">
+                        <div class="p-2 text-center bg-white">
                             <h2 class="text-3xl font-extrabold text-gray-700 md:text-5xl">H. MIFTAH S.H.I</h2>
                         </div>
-                        <div class="flex items-center h-auto p-2 text-center bg-gray-300">
+                        <div class="p-2 text-center bg-gray-300">
                             <h2 class="text-xl font-semibold text-gray-700 md:text-3xl">CALEG DPR RI KALBAR 1</h2>
                         </div>
                     </div>
@@ -62,7 +70,7 @@
             </div>
         </div>
 
-        <div class="absolute top-0 bottom-0 w-full h-full"
+        <div class="absolute top-0 bottom-0 w-full h-full md:h-screen"
             style="background-image: url({{ asset('assets/tortoise-shell.svg') }}); min-height: -webkit-fill-available;">
         </div>
     </section>
@@ -493,7 +501,7 @@
             </div>
         </div>
     </section>
-    {{-- <section class="relative bg-white dark:bg-gray-900">
+    <section class="relative bg-white dark:bg-gray-900">
         <div class="z-40 max-w-screen-md px-4 py-8 mx-auto lg:py-16">
             <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-center text-gray-900 dark:text-white">Hubungi
                 Kami</h2>
@@ -527,7 +535,10 @@
                     message</button>
             </form>
         </div>
-    </section> --}}
+        {{-- <div class="hidden md:block">
+            <svg class="absolute z-20 w-auto h-96 top-14 -left-64" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000" fill="#D3E4CD"><path d="M1790.25 758.43c-83.89-127.72-435-323-545-495-205.29-321.01-400-267-601.54 15.65-196.77 275.94-243.45 319.35-380.45 460.35-155.75 160.29-199.8 254.36-84.44 481.41 84.04 165.4 355.27 293.71 513.44 435.59 212.03 190.19 409.1 435.88 557.69 207.29 258.06-397 415.11-409.44 525.31-572.29 118.15-174.6 168.97-298.59 15-533Z"></path></svg>
+        </div> --}}
+    </section>
     <footer
         class="z-40 p-4 antialiased rounded-lg shadow-2xl shadow-indigo-500/40 bg-gray-50 sm:flex sm:items-center sm:justify-between sm:p-6 xl:p-8 dark:bg-gray-800">
         <p class="mb-4 text-sm text-center text-gray-500 dark:text-gray-400 sm:mb-0">
@@ -563,6 +574,36 @@
             <div id="tooltip-twitter" role="tooltip"
                 class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                 Follow us on Twitter
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <a href="#" data-tooltip-target="tooltip-github"
+                class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                        d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Github</span>
+            </a>
+            <div id="tooltip-github" role="tooltip"
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Star us on GitHub
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+            <a href="#" data-tooltip-target="tooltip-dribbble"
+                class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer dark:text-gray-400 dark:hover:text-white hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                        d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span class="sr-only">Dribbble</span>
+            </a>
+            <div id="tooltip-dribbble" role="tooltip"
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Follow us on Dribbble
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
         </div>
