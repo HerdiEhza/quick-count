b04e06db1c492c1724aaadc5d4f3b74fe6611548
HerdiEhza <herdi.ehza@icloud.com>
2023-11-28 22:25:26 +0700

    remove method post

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -32,7 +32,7 @@ new #[Layout('layouts.guest')] class extends Component
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form wire:submit="login" method="POST">
+    <form wire:submit="login">
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
