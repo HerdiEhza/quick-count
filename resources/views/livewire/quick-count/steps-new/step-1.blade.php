8f03df2565d1dbd98f4b685c97bf4503617abd12
HerdiEhza <91470474+HerdiEhza@users.noreply.github.com>
2024-02-14 15:03:51 +0700

    .

--- resources/views/livewire/quick-count/steps-new/step-1.blade.php
+++ resources/views/livewire/quick-count/steps-new/step-1.blade.php
@@ -26,7 +26,7 @@
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option selected>Harap pilih kecamatan</option>
         @forelse ($kecamatans as $kecamatan)
-        <option value="{{ $kecamatan->id }}">{{ $kecamatan->kecamatan }}
+        <option value="{{ $kecamatan->id }}">{{ $kecamatan->nama_kecamatan }}
         </option>
         @empty
         <option>Tidak ada Kelurahan Desa</option>
